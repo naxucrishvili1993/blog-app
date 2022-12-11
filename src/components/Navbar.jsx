72a803e2e5d7122b6d997c9c7bf2cf3811e054fe
naxucrishvili1993 <giorginaxucrishvili70@gmail.com>
2022-12-11 23:36:14 +0400

    Remove toggler and home button from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,6 @@ const Navbar = (props) => {
 				<a class="navbar-brand" href="#">
 					"G"acebook
 				</a>
-				<button
-					class="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarCollapse"
-					aria-controls="navbarCollapse"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarCollapse">
-					<ul class="navbar-nav ms-auto mb-2 mb-md-0">
-						<li class="nav-item me-auto ms-1">
-							<a class="nav-link active" aria-current="page" href="#">
-								Home
-							</a>
-						</li>
-					</ul>
-				</div>
 			</div>
 		</nav>
 	);
